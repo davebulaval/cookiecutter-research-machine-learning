92cd875b082d20416121b65d7f73f1167186a022
Dominique <dpothier@live.ca>
2020-04-08 09:11:27 -0400

    Removed test remote url

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,11 +6,6 @@ conda_env_creation = True if "{{ cookiecutter.create_conda_env }}" == "Yes" else
 data_version_control = True if "{{ cookiecutter.DVC_setting }}" == "Yes" else False
 
 
-try:
-    subprocess.run(["make", "test_remote_url"], check=True)
-except subprocess.CalledProcessError:
-    sys.exit(1)
-
 if conda_env_creation:
     try:
         subprocess.run(["make", "init_environment"], check=True)
