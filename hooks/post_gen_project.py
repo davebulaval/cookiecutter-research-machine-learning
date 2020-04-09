3cfe34e508a54d50a06eb15353a83f945fe39d5a
Dominique <dpothier@live.ca>
2020-04-09 13:36:52 -0400

    Made DVC mandatory

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,7 +3,6 @@ import subprocess
 import sys
 
 venv_env_creation = True if "{{ cookiecutter.create_python_venv }}" == "Yes" else False
-data_version_control = True if "{{ cookiecutter.DVC_setting }}" == "Yes" else False
 init_git = True if "{{ cookiecutter.init_git }}" == "Yes" else False
 
 if venv_env_creation:
@@ -15,11 +14,5 @@ if venv_env_creation:
 if init_git:
     try:
         subprocess.run(["make", "init_git"], check=True)
-    except subprocess.CalledProcessError:
-        sys.exit(1)
-
-if data_version_control:
-    try:
-        subprocess.run(["make", "init_dvc"], check=True)
     except subprocess.CalledProcessError:
         sys.exit(1)
